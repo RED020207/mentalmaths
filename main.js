491fa42f7706e002834894e9a3b9394ff5503628
RED020207 <priyanshubanerjee39@gmail.com>
2026-05-04 20:11:54 +0530

    Added input field, timer and partial maths in maths.html - 04/05/2026 20:11 PM

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-entry = document.getElementById("entry");
-entry.addEventListener("click", function() {
+let entry = document.getElementById("entry");
+let time = document.getElementById("time");
+let t = 10;
+const answer = document.getElementById("answer");
+const input = document.getElementById("input");
+if (entry){
+    entry.addEventListener("click", function() {
     window.location.href = "maths.html";
-});
+    });
+}
+if (answer){
+    answer.addEventListener("submit", function() {
+        event.preventDefault();
+        console.log("Answer submitted: " + input.value);
+        input.value = "";
+    }
+    );
+}
+function startTimer() {
+    const timer = setInterval(() => {
+        time.innerHTML = t;
+        t--;
+        if (t < 0) {
+            t = 10;
+            clearInterval(timer);
+            startTimer();
+        }
+    }, 1000);
+}
+
+startTimer();
